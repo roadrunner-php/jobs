587855e32760165b5c08ba091dc819a642d063ab
SerafimArts <nesk@xakep.ru>
2021-09-02 20:21:09 +0300

    Rename ephemeral to memory driver

--- src/Queue/EphemeralCreateInfo.php
+++ src/Queue/EphemeralCreateInfo.php
@@ -12,46 +12,9 @@ declare(strict_types=1);
 namespace Spiral\RoadRunner\Jobs\Queue;
 
 /**
- * The DTO to create the Ephemeral driver.
- *
- * @psalm-import-type CreateInfoArrayType from CreateInfoInterface
+ * @deprecated This class has been renamed to "MemoryCreateInfo". Please
+ *             use {@see MemoryCreateInfo} instead.
  */
-final class EphemeralCreateInfo extends CreateInfo
+final class EphemeralCreateInfo extends MemoryCreateInfo
 {
-    /**
-     * @var positive-int
-     */
-    public const PREFETCH_DEFAULT_VALUE = 10;
-
-    /**
-     * @var positive-int
-     */
-    public int $prefetch = self::PREFETCH_DEFAULT_VALUE;
-
-    /**
-     * @param non-empty-string $name
-     * @param positive-int $priority
-     * @param positive-int $prefetch
-     */
-    public function __construct(
-        string $name,
-        int $priority = self::PRIORITY_DEFAULT_VALUE,
-        int $prefetch = self::PREFETCH_DEFAULT_VALUE
-    ) {
-        parent::__construct(Driver::EPHEMERAL, $name, $priority);
-
-        assert($prefetch >= 1, 'Precondition [prefetch >= 1] failed');
-
-        $this->prefetch = $prefetch;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function toArray(): array
-    {
-        return \array_merge(parent::toArray(), [
-            'prefetch' => $this->prefetch,
-        ]);
-    }
 }
