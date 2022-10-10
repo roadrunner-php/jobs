60eba60f3d4354e5e9a2c86f3014dcc8ab715e83
butschster <butschster@gmail.com>
2022-10-10 19:45:17 +0400

    Small fix

--- src/Consumer.php
+++ src/Consumer.php
@@ -123,7 +123,7 @@ final class Consumer implements ConsumerInterface, SerializerAwareInterface
      */
     private function getPayload(Payload $payload): array
     {
-        if (empty($payload->body)) {
+        if ($payload->body === '' || $payload->body === null) {
             return [];
         }
 
