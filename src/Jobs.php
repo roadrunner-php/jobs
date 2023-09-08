da35a4ed16c1f0ac53f75be77a70d5e935878a47
butschster <butschster@gmail.com>
2023-09-08 15:25:11 +0400

    Fixes psalm errors

--- src/Jobs.php
+++ src/Jobs.php
@@ -111,6 +111,7 @@ final class Jobs implements JobsInterface
                 \is_int($value),
                 \is_string($value),
                 $value instanceof \Stringable,
+                // @psalm-suppress RedundantCast
                 \is_object($value) && \method_exists($value, '__toString') => (string)$value,
                 \is_bool($value) => $value ? 'true' : 'false',
                 $value instanceof \JsonSerializable,
